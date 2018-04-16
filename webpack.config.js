a238f1f4f6d4927ce550d847f3ff2918bf0eebad
lijiapeng001 <1146417576@qq.com>
2018-04-16 10:04:00 +0800

    webpack配置

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,16 @@
  * @Author: mikey.zhaopeng 
  * @Date: 2018-04-15 14:52:42 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2018-04-15 18:00:33
+ * @Last Modified time: 2018-04-16 09:58:56
  */
 var webpack = require("webpack");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+//环境变量的配置  dev-online
+var WEBPACK_ENV = process.env.WEBPACK_ENV || 'online';
+console.log(WEBPACK_ENV);
+
 //获取html-webpack-plugins
 var getHtml = function (name) {
     return {
@@ -19,18 +24,19 @@ var getHtml = function (name) {
 }
 var configs = {
     entry:{
-        "common" : ["./src/page/common/common.js"],
+        "common" : ["./src/page/common/common.js",],
         "index" : ["./src/page/index/index.js"],
         "login" : ["./src/page/login/login.js"]
     },
     output:{
         path:"./dist",
+        publicPath:"/dist",
         filename:"js/[name].js"
     },
     module: {
         loaders: [
           { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader","css-loader") },
-          { test: /\.(gif|png|jpg)\??.*$/, loader: "url-loader?limit=100&name=res/[name].[ext]" }
+          { test: /\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/, loader: "url-loader?limit=100&name=../res/[name].[ext]" }
         ]
       },
     plugins: [
@@ -45,5 +51,8 @@ var configs = {
         new HtmlWebpackPlugin( getHtml('index')  ),
         new HtmlWebpackPlugin( getHtml('login')  ),
     ]
+}
+if("dev" === WEBPACK_ENV){
+    config.entry.common.push("webpack-dev-server/client?http://localhost:8088/");
 }
 module.exports = configs
